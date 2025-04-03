f7967c0f19577d358b29c6c8ae0967814358a465
rod <dev_rodts@outlook.com>
2025-04-02 22:17:23 -0300

    refactor: refactoring product test

--- src/main/java/com/devrodts/commerce/commerce/modules/product/application/dtos/CreateProductDTO.java
+++ src/main/java/com/devrodts/commerce/commerce/modules/product/application/dtos/CreateProductDTO.java
@@ -2,7 +2,7 @@ package com.devrodts.commerce.commerce.modules.product.application.dtos;
 
 import java.math.BigDecimal;
 
-public record CreateProductRequestDTO(
+public record CreateProductDTO(
         String productName,
         String productDescription,
         BigDecimal productPrice,
--- src/main/java/com/devrodts/commerce/commerce/modules/product/infrastructure/repository/ProductRepository.java
+++ src/main/java/com/devrodts/commerce/commerce/modules/product/infrastructure/repository/ProductRepository.java
@@ -1,23 +1,9 @@
 package com.devrodts.commerce.commerce.modules.product.infrastructure.repository;
-import com.devrodts.commerce.commerce.modules.category.domain.entity.CategoryEntity;
-import com.devrodts.commerce.commerce.modules.image.domain.entity.ImageEntity;
-
-import java.math.BigDecimal;
-import java.util.List;
+import com.devrodts.commerce.commerce.modules.product.domain.entity.ProductEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
-public class ProductRepository {
-
-    private UUID productId;
-    private String productName;
-    private String productDescription;
-    private BigDecimal productPrice;
-    private int productQuantity;
-    private String productBrand;
-    private CategoryEntity category;
-    private List<ImageEntity> images;
-
-
-
-
+@Repository
+public interface ProductRepository extends JpaRepository<ProductEntity, UUID> {
 }
