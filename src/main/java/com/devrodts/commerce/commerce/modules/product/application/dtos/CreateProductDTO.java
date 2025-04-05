cfd39ed2a87a2434c016bd00ef16b4b536600830
rod <dev_rodts@outlook.com>
2025-04-05 08:03:49 -0300

    "refactor: create product dto"

--- src/main/java/com/devrodts/commerce/commerce/modules/product/application/dtos/CreateProductDTO.java
+++ src/main/java/com/devrodts/commerce/commerce/modules/product/application/dtos/CreateProductDTO.java
@@ -1,12 +1,16 @@
 package com.devrodts.commerce.commerce.modules.product.application.dtos;
 
 import java.math.BigDecimal;
+import java.util.List;
+
+import com.devrodts.commerce.commerce.modules.image.domain.entity.ImageEntity;
 
 public record CreateProductDTO(
-        String productName,
-        String productDescription,
-        BigDecimal productPrice,
-        int productQuantity,
-        String productBrand
+        String name,
+        String description,
+        BigDecimal price,
+        int stock,
+        String brand,
+        List<ImageEntity> images
 ) {
 }
