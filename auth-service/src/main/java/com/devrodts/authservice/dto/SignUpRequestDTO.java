3fd1df68d1bbe2d170de3e2ed87cb986289c35a9
rod <dev_rodts@outlook.com>
2025-04-08 07:54:45 -0300

    feat: add SignUpResponseDTO

--- auth-service/src/main/java/com/devrodts/authservice/dto/SignUpRequestDTO.java
+++ auth-service/src/main/java/com/devrodts/authservice/dto/SignUpRequestDTO.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public record LoginRequestDTO(
+public record SignUpRequestDTO(
         @Schema(
                 description = "useremail@email.com",
                 example = "useremail@email.com"
